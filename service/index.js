d42df9a238edc767ccccf53ba75df166fb26b808
Manisha Rath <rathmanisha889@gmail.com>
2024-03-16 03:00:34 +0530

    connet to database

--- service/index.js
+++ service/index.js
@@ -1,5 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { error } from "console";
+dotenv.config();
 
+mongoose.connect(process.env.MONGO)
+.then(() => {
+    console.log("connected");
+})
+.catch((err) => {
+    console.log(err);
+})
 const app = express();
 
 app.listen(3000, ()=> {
