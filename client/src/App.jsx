21f907d9e3d2140b1f3a73bd0be8087e6c2cdfa7
Manisha Rath <rathmanisha889@gmail.com>
2024-03-16 02:03:49 +0530

    Created Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,17 +4,21 @@ import About from './Pages/About';
 import LoginPage from './Pages/LoginPage';
 import Profile from './Pages/Profile';
 import SignUp from './Pages/SignUp';
+import Navbar from './components/Navbar';
+import Contact from './Pages/Contact';
 
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Navbar/>
      <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/profile" element={<Profile />} />
-      <Route path="/signOut" element={<SignUp />} />
+      <Route path="/signUp" element={<SignUp />} />
+      <Route path="/contact" element={<Contact />} />
 
      </Routes>
     </BrowserRouter>
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import {FaSearch} from 'react-icons/fa';
+import {Link} from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <header className='bg bg-slate-200 shadow-md'>
+        <div className='flex justify-between items-center max-w-16xl mx-auto p-3'>
+
+            <Link to="/">
+            <h1 className='font-extrabold text-sm sm:text-xl flex flex-wrap'>
+                <span className='text-slate-500 '>Real</span>
+                <span className='text-slate-600'>Estate</span>
+            </h1>
+            </Link>
+
+            <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+                <input type="text" placeholder="Search..." className='bg-transparent focus:outline-none w-24 sm:w-64'/>
+                <FaSearch className='text-slate-500'/>
+            </form>
+
+            <ul className='flex gap-6'>
+            <Link to="/">
+                <li className='font-semibold hidden sm:inline text-indigo-500 hover:italic 
+                hover:text-lime-600 hover:font-bold'>Home</li>
+            </Link>
+
+            <Link to="/about">
+                <li className='font-semibold hidden sm:inline text-indigo-500 hover:italic 
+                hover:text-lime-600  hover:font-bold'>About</li>
+            </Link>
+
+            <Link to="/contact">
+                <li className='font-semibold hidden sm:inline text-indigo-500 hover:italic 
+                hover:text-lime-600  hover:font-bold'>Contact</li>
+            </Link>
+
+            <Link to="/login">
+                <li className='font-semibold sm:inline text-indigo-500 hover:italic 
+                hover:text-lime-600 hover:font-bold'>Login</li>
+            </Link>  
+            </ul>
+        </div>
+
+    </header>
+  )
+}
