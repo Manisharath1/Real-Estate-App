c7800fa7d40526eda13fd473cecc013e8dd5a72d
Manisha Rath <rathmanisha889@gmail.com>
2024-03-15 13:56:56 +0530

    Routing Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import LoginPage from './Pages/LoginPage';
+import Profile from './Pages/Profile';
+import SignUp from './Pages/SignUp';
+
 
 export default function App() {
   return (
-    <h1 className='text-red-600'>
-     App 
-    </h1>
+    <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/signOut" element={<SignUp />} />
+
+     </Routes>
+    </BrowserRouter>
   )
 }
