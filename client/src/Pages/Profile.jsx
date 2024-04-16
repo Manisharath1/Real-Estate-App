e6957e09b716f4b745cdfa944872e35fd9c8c312
Manisha Rath <rathmanisha889@gmail.com>
2024-04-17 00:39:55 +0530

    Delete user

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux"
 import { useRef, useState, useEffect } from "react"
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage';
 import { app } from "../firebase";
-import { updateUserStart, updateUserSuccess, updateUserFailure } from "../redux/user/userSlice";
+import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess } from "../redux/user/userSlice";
 import { useDispatch} from "react-redux";
 
 export default function Profile() {
@@ -78,7 +78,23 @@ export default function Profile() {
       dispatch(updateUserFailure(error.message));
     }
   }
-
+  const handleDeleteUser = async() =>{
+      try {
+        dispatch(deleteUserStart());
+        const res = await fetch(`/service/user/delete/${currentUser._id}`,
+        {
+          method: 'DELETE',
+        });
+        const data = await res.json();
+        if(data.success === false){
+          dispatch(deleteUserFailure(data.message));
+          return;
+        }
+        dispatch(deleteUserSuccess(data)); 
+      } catch (error) {
+        dispatch(deleteUserFailure(error.message))
+      }
+    };
 
   return (
 
@@ -121,7 +137,7 @@ export default function Profile() {
 
       <div className=" flex justify-between mt-6">
 
-        <span className="text-red-800 cursor-pointer font-bold">Delete Account</span>
+        <span onClick={handleDeleteUser} className="text-red-800 cursor-pointer font-bold">Delete Account</span>
         <span className="text-red-800 text-right font-bold cursor-pointer">Logout</span>
 
       </div>
